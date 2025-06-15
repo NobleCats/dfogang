a10588ec34d01522b651c81b1f4b710212f90bad
NobleCat <noblecat@farseer.ai>
2025-06-14 23:26:27 -0400

    Label

--- ui.js
+++ ui.js
@@ -92,24 +92,21 @@ function renderDpsCalculatorWidget(profile, equipment, setItemInfo, dpsState) {
         }
     }
 
-    if (isCleansingSetEquipped) {
-        container.innerHTML += `
-            <div class="dps-toggle-group">
-                <div class="dps-toggle-label">
-                    Set Normalize
-                    <span class="tooltip-icon">?</span>
-                    <div class="tooltip-content">
-                        <p>Calculates damage based on a comparable tier of the "Death in the Shadows" Set.</p>
-                        <p>(This feature was added to provide a rough standard, as object damage is not reflected in DPS calculations.)</p>
-                    </div>
-                </div>
-                <div class="dps-toggle-switch">
-                    <div class="dps-toggle-option ${dpsOptions.average_set_dmg ? 'active' : ''}" data-dps-option="average_set_dmg" data-dps-value="true">On</div>
-                    <div class="dps-toggle-option ${!dpsOptions.average_set_dmg ? 'active' : ''}" data-dps-option="average_set_dmg" data-dps-value="false">Off</div>
+    container.innerHTML += `
+        <div class="dps-toggle-group">
+            <div class="dps-toggle-label">
+                Set Normalize
+                <span class="tooltip-icon">?</span> <div class="tooltip-content">
+                    <p>Calculates damage based on a comparable tier of the Death in the Shadows Set.</p>
+                    <p>(This feature was added to provide a rough standard, as object damage is not reflected in DPS calculations.)</p>
                 </div>
             </div>
-        `;
-    }
+            <div class="dps-toggle-switch">
+                <div class="dps-toggle-option ${dpsOptions.average_set_dmg ? 'active' : ''}" data-dps-option="average_set_dmg" data-dps-value="true">On</div>
+                <div class="dps-toggle-option ${!dpsOptions.average_set_dmg ? 'active' : ''}" data-dps-option="average_set_dmg" data-dps-value="false">Off</div>
+            </div>
+        </div>
+    `;
 
     const weapon = equipment.find(eq => eq.slotId === 'WEAPON');
     const weaponName = weapon?.itemName || "";
