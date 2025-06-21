ec07a18d654f116408c56e271d991148661a1748
NobleCat <richwjddn@gmail.com>
2025-06-21 07:49:03 -0400

    Revert "checkbox"
    This reverts commit 40244e168e5040758644784f0723a853c5fcc1a1.

--- app.js
+++ app.js
@@ -83,11 +83,6 @@ function render() {
             }
         }
         ui.showMoreResultsIndicator(state.displayedResults.length < state.allSearchResults.length);
-        
-        if (searchOptionsDiv) {
-            searchOptionsDiv.style.display = state.server === 'explorer' ? 'flex' : 'none';
-            exactMatchCheckbox.checked = state.exactMatch;
-        }
 
     } else if (state.view === 'detail' && state.characterDetail.profile) {
         ui.renderCharacterDetail(
@@ -368,14 +363,6 @@ async function init() {
         });
     }
 
-    exactMatchCheckbox.addEventListener('change', () => {
-        if (state.searchTerm) {
-            handleSearchClick(); 
-        }
-    });
-
-    serverSelect.addEventListener('change', handleServerSelectChange);
-
     const resultsSection = document.querySelector('.results-section');
     if (resultsSection) {
         resultsSection.addEventListener('scroll', handleScroll);
@@ -402,8 +389,6 @@ async function init() {
         } else {
             await performSearch(server, name);
         }
-    } else {
-        handleServerSelectChange(); 
     }
     setupAccordions();
     render();
